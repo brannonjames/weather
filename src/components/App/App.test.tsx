3e77f0a4d44571a17d9942d96b97b99257a27a1e
Jimmy <jimmy.brannon@barometerit.com>
2020-11-24 16:40:27 -0600

    add test for checking if env exists

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -7,3 +7,7 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('Environment variables exist', () => {
+  expect(process.env.REACT_APP_WEATHER_API_KEY).toBeTruthy();
+});
